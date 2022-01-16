b32b86cfbdfa088d084438acf99a7df12c1f7204
ve5li <ve5li@tuta.io>
2022-01-16 01:18:30 +0100

    add custom keymap

---
+++ custom/config.h
@@ -0,0 +1,34 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* overwrite pins to match the teensy 2.0 */
+#undef MATRIX_COL_PINS
+#define MATRIX_COL_PINS { C6, D7, C7, B4, B5 }
+
+/* Use I2C or Serial, not both */
+#define USE_SERIAL
+
+/* Select hand configuration */
+#define MASTER_LEFT
+
+/* flip thumb clusters */
+#define FLIP_HALF
