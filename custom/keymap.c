180c387f801accdc4501e456e9188a2808db2ea0
ve5li <ve5li@tuta.io>
2022-01-18 02:43:40 +0100

    update keyboard layout

--- custom/keymap.c
+++ custom/keymap.c
@@ -4,10 +4,10 @@
 #define _RAISE 1
 #define _LOWER 2
 
-#define GUI_ESC  GUI_T(KC_ESC)
-#define CTL_BSPC CTL_T(KC_BSPC)
-#define SFT_SPC  SFT_T(KC_SPC)
-#define ALT_ENT  ALT_T(KC_ENT)
+#define CTL_ESC    CTL_T(KC_ESC)
+#define RAISE_SPC  MT(_RAISE, KC_SPC)
+#define LOWER_BSPC MT(_LOWER, KC_BSPC)
+#define SHIFT_ENT  SFT_T(KC_ENT)
 
 #define KC_ML KC_MS_LEFT
 #define KC_MR KC_MS_RIGHT
@@ -30,24 +30,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+-------------,                             ,-------------+------+------+------,
      *        |  [   |   ]  |                                                         |   -  |   =  |
      *        '------+------'-------------'                             '-------------'------+------'
-     *                      | ESC  |  BS  |                             | SPACE|ENTER |
+     *                      | ESC  |SPACE |                             |  BS  |ENTER |
      *                      |  +   |   +  |                             |  +   |  +   |
-     *                      | GUI  | CTRL |                             | SHIFT| ALT  |
+     *                      | CTRL |Raise |                             | Lower|SHIFT |
      *                      '------+------'                             '------+------'
      *                                    '------+------' '------+------'
-     *                                    |  TAB |Raise | | Lower| DEL  |
+     *                                    |  TAB | GUI  | |  ALT | DEL  |
      *                                    '------+------' '------+------'
-     *                                    | HOME |  ~   | | GUI  | END  |
+     *                                    | HOME |  ~   | |INSERT| END  |
      *                                    '------+------' '------+------'
      */
+
     [_BASE] = LAYOUT(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT,
                           KC_LBRC, KC_RBRC,                                                        KC_MINS, KC_EQL,
-                                                     GUI_ESC, CTL_BSPC, SFT_SPC, ALT_ENT,
-                                                     KC_TAB,  RAISE,    KC_END,  KC_DEL,
-                                                     KC_HOME, KC_GRV,   KC_LGUI, LOWER
+                                                     CTL_ESC, RAISE_SPC, LOWER_BSPC, SHIFT_ENT,
+                                                     KC_TAB,  KC_LGUI,   KC_LALT,    KC_DEL,
+                                                     KC_HOME, KC_GRV,    KC_INSERT,  KC_END
     ),
 
     /* Raise
